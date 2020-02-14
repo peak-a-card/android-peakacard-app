f30456edd50b428beea759f35bb833ba216d9261
Sergi Castillo <sergi.castillo@adevinta.com>
2020-02-15 00:27:14 +0100

    Dark mode

--- app/src/main/java/com/peakacard/app/PeakACardApplication.kt
+++ app/src/main/java/com/peakacard/app/PeakACardApplication.kt
@@ -1,6 +1,8 @@
 package com.peakacard.app
 
 import android.app.Application
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.emoji.bundled.BundledEmojiCompatConfig
 import androidx.emoji.text.EmojiCompat
 
@@ -10,5 +12,11 @@ class PeakACardApplication : Application() {
         super.onCreate()
         val config = BundledEmojiCompatConfig(this)
         EmojiCompat.init(config)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+        }
     }
 }
