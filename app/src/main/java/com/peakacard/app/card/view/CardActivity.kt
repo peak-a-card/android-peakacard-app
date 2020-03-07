230ec1b26dc25f83b1ab09337e29dc7ced83013c
Sergi Castillo <sergi.castillo@adevinta.com>
2020-03-07 20:57:31 +0100

    Add drag image motion

--- app/src/main/java/com/peakacard/app/card/view/CardActivity.kt
+++ app/src/main/java/com/peakacard/app/card/view/CardActivity.kt
@@ -1,15 +1,17 @@
 package com.peakacard.app.card.view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.emoji.widget.EmojiTextView
 import com.peakacard.app.R
 import com.peakacard.app.cards.view.model.Card
 import com.peakacard.app.extensions.applyCardText
 import com.peakacard.core.ui.extensions.bindView
-import com.peakacard.core.ui.extensions.fromHtmlCompat
 
 class CardActivity : AppCompatActivity() {
 
@@ -30,11 +32,45 @@ class CardActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card)
 
         cardDetail.applyCardText(card)
         cardDetail.transitionName = this@CardActivity.transitionName
+
+        val height = resources.getDimensionPixelSize(R.dimen.card_detail_height)
+        val heightLimit = height * 0.6
+        var dY = 0f
+        var startY = 0f
+        cardDetail.setOnTouchListener { view, motionEvent ->
+            return@setOnTouchListener when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    startY = view.y
+                    dY = view.y - motionEvent.rawY
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    view.y = motionEvent.rawY + dY
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    if (view.y <= -heightLimit) {
+                        view.animate().y(-height.toFloat()).apply {
+                            duration = resources.getInteger(R.integer.anim_duration_short).toLong()
+                            interpolator = AccelerateDecelerateInterpolator()
+                        }
+                    } else {
+                        view.animate().y(startY).apply {
+                            duration = resources.getInteger(R.integer.anim_duration_short).toLong()
+                            interpolator = AccelerateDecelerateInterpolator()
+                        }
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
