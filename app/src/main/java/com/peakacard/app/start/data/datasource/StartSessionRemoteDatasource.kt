8e9342b44acda13ff3eaee2a99e0ab353c0d9b92
Sergi Castillo <sergi.castillo@adevinta.com>
2020-03-14 16:32:08 +0100

    Update field

--- app/src/main/java/com/peakacard/app/start/data/datasource/StartSessionRemoteDatasource.kt
+++ app/src/main/java/com/peakacard/app/start/data/datasource/StartSessionRemoteDatasource.kt
@@ -2,6 +2,7 @@ package com.peakacard.app.start.data.datasource
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
+import com.peakacard.app.start.data.model.SessionDataModel
 import com.peakacard.app.start.data.model.StartSessionRequestDataModel
 import com.peakacard.app.start.data.model.StartSessionResponseDataModel
 import com.peakacard.core.Either
@@ -17,9 +18,9 @@ class StartSessionRemoteDatasource(private val database: FirebaseFirestore) {
             val sessionId = startSessionRequest.id.value
             val dbSession = sessions.document(sessionId).get().await()
             if (dbSession.exists()) {
-                val participants = dbSession.get("participants")
+                val participants = dbSession.get(SessionDataModel.PARTICIPANTS)
                 (participants as MutableList<String>).add(startSessionRequest.participant.value)
-                sessions.document(sessionId).set(hashMapOf("participants" to participants))
+                sessions.document(sessionId).update(SessionDataModel.PARTICIPANTS, participants)
                 Either.Right(StartSessionResponseDataModel.Success)
             } else {
                 Either.Left(StartSessionResponseDataModel.Error.NoSessionFound)
