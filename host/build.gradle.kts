206ca7f8aa351609368617a0360d08188739fa70
Sergi Castillo <sergi.castillo@adevinta.com>
2020-04-17 12:26:03 +0200

    Create host module

---
+++ host/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(Versions.androidCompileSdkVersion)
+
+    defaultConfig {
+        applicationId = "com.peakacard.app"
+        setTargetSdkVersion(Versions.androidTargetSdkVersion)
+        setMinSdkVersion(Versions.androidMinSdkVersion)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xuse-experimental=kotlin.Experimental"
+        freeCompilerArgs = freeCompilerArgs + "-Xuse-experimental=kotlinx.coroutines.FlowPreview"
+        freeCompilerArgs = freeCompilerArgs + "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
+        freeCompilerArgs = freeCompilerArgs + "-XXLanguage:+InlineClasses"
+    }
+}
+
+dependencies {
+    implementation(project(":core-ui"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+include(":host")
 include(
     "bom",
     "core",
