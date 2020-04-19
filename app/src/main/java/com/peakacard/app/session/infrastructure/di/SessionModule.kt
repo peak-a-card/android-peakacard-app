0b64c1d0fdbd86fa18e9628270f9b14d73db8a4a
Sergi Castillo <sergi.castillo@adevinta.com>
2020-04-19 12:45:15 +0200

    FIX duplicate dependencies

--- app/src/main/java/com/peakacard/app/session/infrastructure/di/SessionModule.kt
+++ app/src/main/java/com/peakacard/app/session/infrastructure/di/SessionModule.kt
@@ -1,34 +1,12 @@
 package com.peakacard.app.session.infrastructure.di
 
-import android.content.Context
-import com.peakacard.session.data.datasource.local.SessionLocalDataSource
-import com.peakacard.session.data.datasource.remote.SessionRemoteDataSource
-import com.peakacard.session.data.repository.SessionRepository
 import com.peakacard.app.session.domain.JoinSessionUseCase
 import com.peakacard.app.session.domain.LeaveSessionUseCase
 import com.peakacard.app.session.view.JoinSessionViewModel
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val sessionModule = module {
     factory { JoinSessionViewModel(get(), get()) }
     factory { JoinSessionUseCase(get(), get()) }
-    factory { SessionRepository(get(), get(), get()) }
-    factory {
-        SessionRemoteDataSource(
-            get()
-        )
-    }
-    factory { SessionLocalDataSource(get(named(SESSION_SHARED_PREFERENCES))) }
-    factory(named(SESSION_SHARED_PREFERENCES)) {
-        androidContext().getSharedPreferences(
-            SESSION_SHARED_PREFERENCES,
-            Context.MODE_PRIVATE
-        )
-    }
-
     factory { LeaveSessionUseCase(get(), get()) }
 }
-
-private const val SESSION_SHARED_PREFERENCES = "SESSION_SHARED_PREFERENCES"
--- host/src/main/java/com/peakacard/host/session/infrastructure/di/CreateSessionModule.kt
+++ host/src/main/java/com/peakacard/host/session/infrastructure/di/CreateSessionModule.kt
@@ -2,11 +2,9 @@ package com.peakacard.host.session.infrastructure.di
 
 import com.peakacard.host.session.domain.CreateSessionUseCase
 import com.peakacard.host.session.view.CreateSessionViewModel
-import com.peakacard.user.view.model.mapper.FirebaseUserMapper
 import org.koin.dsl.module
 
 val createSessionModule = module {
     factory { CreateSessionViewModel(get()) }
     factory { CreateSessionUseCase(get()) }
-    factory { FirebaseUserMapper() }
 }
