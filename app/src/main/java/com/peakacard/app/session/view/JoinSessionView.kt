7a9622e1e24cab21461c8626a2a3638575c52868
Sergi Castillo <sergi.castillo@adevinta.com>
2020-04-22 20:41:03 +0200

    Use PeakViewModel

--- app/src/main/java/com/peakacard/app/session/view/JoinSessionView.kt
+++ app/src/main/java/com/peakacard/app/session/view/JoinSessionView.kt
@@ -1,7 +1,6 @@
 package com.peakacard.app.session.view
 
 import com.peakacard.app.session.view.state.JoinSessionState
+import com.peakacard.core.view.PeakView
 
-interface JoinSessionView {
-  fun updateState(state: JoinSessionState)
-}
+interface JoinSessionView : PeakView<JoinSessionState>
